64d388354023ed7cf2b2fd4a87e4181c73b39316
hilker bazan flores <201914341d@utea.edu.pe>
2024-07-17 17:04:39 -0500

    cambiando el acceso al controlador de laravel

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
+//esta funcion muestra la vista
+/*
 Route::get('/', function () {
     return view('task.index');
 });
+*/
 
+//recepciona los datos del metodo post
+Route::get('/', [TaskController::class, 'index']);
 Route::post('/', function () {
     print_r($_POST);
 });
