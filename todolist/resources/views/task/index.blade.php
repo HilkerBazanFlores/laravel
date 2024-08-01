a659a8cd1f7eb96ea9767a098b1c57d8bcb1e009
hilker bazan flores <201914341d@utea.edu.pe>
2024-07-31 23:00:40 -0500

    agregamos algunos estilos con la ayuda de botstrap 5, esto nos ayudara a mejorar la interface CRUD de nuestro programa

--- todolist/resources/views/task/index.blade.php
+++ todolist/resources/views/task/index.blade.php
@@ -1,41 +1,102 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Aplicacion Todo List en laravel</title>
-</head>
-<body>
-  
-<form action="{{ url('/') }}" method="post">
-@csrf
-<input type="text" name="task" id="task">
+  <head>
+    <title>Aplicación todolist con laravel</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
 
-<input type="button" value="Agregar tarea">
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+  </head>
+
+  <body>
+    <header>
+      <main class="container">
+<br/>
+    </header>
+
+    <div class="card">
+      <div class="card-header">Tareas:</div>
+      <div class="card-body">
+      <form action="{{ url('/') }}" method="post">
+@csrf
+Tarea:
+<br/>
+<input type="text" class="form-control" name="task" id="task">
+<br/>
+<input type="button" class="btn btn-primary" value="Agregar tarea">
 
 </form>
+<br/>
+</div>
+<div class="card-footer text-muted" ></div>
+<div>
+     
 
-<table border="1">
-  <tr>
-    <td> Nombre de la tarea </td>
-    <td> Acción </td>
-  </tr>
-  
-@foreach($tasks as $task)
 
-  <tr>
-    <td> {{ $task->task }} </td>
-    <td>
-      <form action="{{ route('task.destroy', $task -> id) }}" method="post">
+
+<div
+  class="table-responsive-sm table-borderd table-striped"
+>
+  <table
+    class="table"
+  >
+    <thead>
+      <tr>
+        <th scope="col">Acción</th>
+        <th scope="col">Nombre Tarea</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($tasks as $task)
+
+      <tr class="">
+      <td><form action="{{ route('task.destroy', $task -> id) }}" method="post">
         @csrf
         @method('DELETE')
-        <input type="submit" value="X">
-      </form>
-    </td>
-  </tr>
+        <input type="submit" class="btn btn-danger" value="(x) Realizada">
+      </form></td>
+      <td scope="row">{{ $task->task }}</td>
+        
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
 
-@endforeach
-</table>
+      </div>
+      <div class="card-footer text-muted"></div>
+    </div>
+    
 
-</body>
+
+    <main class="cointainer">
+    
+    </main>
+    <footer>
+      <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+      integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
+
